825e186d87976bd859dd40ee25e594cda839a1f4
LeHuuTri-186 <110376741+LeHuuTri-186@users.noreply.github.com>
2024-04-27 15:06:10 +0700

    docker

--- src/main/java/com/lehuutri/studentmanagement/domain/entities/CourseEntity.java
+++ src/main/java/com/lehuutri/studentmanagement/domain/entities/CourseEntity.java
@@ -1,7 +1,12 @@
 package com.lehuutri.studentmanagement.domain.entities;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,9 +21,15 @@ import lombok.NoArgsConstructor;
 @Table(name = "Course")
 public class CourseEntity {
     @Id
-    private String id;
+    private String courseId;
     private String name;
     private String lecture;
     private Integer year;
     private String note;
+
+    @ManyToMany
+    @JoinTable(name = "Course_Student",
+        joinColumns = @JoinColumn(name = "courseId"),
+        inverseJoinColumns = @JoinColumn(name = "studentId"))
+    private List<StudentEntity> students;
 }
--- src/main/java/com/lehuutri/studentmanagement/domain/entities/StudentEntity.java
+++ src/main/java/com/lehuutri/studentmanagement/domain/entities/StudentEntity.java
@@ -1,9 +1,11 @@
 package com.lehuutri.studentmanagement.domain.entities;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,9 +20,11 @@ import lombok.NoArgsConstructor;
 @Table(name = "Student")
 public class StudentEntity {
     @Id
-    private String id;
+    private String studentId;
     private String name;
     private String address;
     private LocalDate birthday;
     private String note;
+    @ManyToMany(mappedBy = "students")
+    private List<CourseEntity> courses;
 }
